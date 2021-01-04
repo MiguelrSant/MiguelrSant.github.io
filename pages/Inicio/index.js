2af8731279f322d3d4a62da4672f14074786c97d
MiguelrSant <75269821+MiguelrSant@users.noreply.github.com>
2021-01-04 07:45:42 -0300

    jj

--- pages/Inicio/index.js
+++ pages/Inicio/index.js
@@ -1,5 +1,10 @@
+const {titulo1, titulo2, titulo3, titulo4, titulo5, titulo6,
+        texto1, texto2, texto3, texto4, texto5, texto6,
+        img1, img2, img3, img4, img5, img6,} = require('./text.json')
+
 export default () => {
     const container = document.createElement('div')
+
 
     const template = 
     `
@@ -10,159 +15,42 @@ export default () => {
             COM O FREIZEIT   
         </h1>
     </header>
+
+
     <div class="novidades-">
-        <div>
-            <h1 class="tiln">
-                Free Fire
-            </h1>
-        </div>
-        <div>
-            <p class="parn">
-            
-                Para saber todas as mudanças, cada detalhe do que irá <br>
-                mudar após a manutenção do Free Fire nesta quarta feira, dia <br>
-                08 de Abril, acesse: Notas da atualização do Free Fire em Abril
-            </p>
-        </div>
-        <div class="imgs" id="imgfree">
-        </div>
-        <style> 
-        #imgfree { 
-            background-image: url(https://raw.githubusercontent.com/MiguelrSant/IMGO/main/FREIZEIT/Noticias/Free-Fire%20.png?token=AR6INPIDGRUS7ZZPYAEUDVK74EHRA);
-            border-radius: 40px
-        } 
-        </style>
+        <div> <h1 class="tiln">${titulo1}</h1> </div>
+        <div> <p class="parn"> ${texto1} </p> </div>
+        <div style="background-image: url(${img1}); border-radius: 40px;" > </div>
     </div>  
     <br>
     <div class="novidades-">
-        <div>
-            <h1 class="tiln1">
-                Conter Strik
-            </h1>
-            <style>
-                .tiln1 {
-                    color: white;
-                    font-family: Arial; 
-                }
-                .tiln1:hover {
-                    border-bottom: 2px solid white;
-                    width: 250px
-                }
-            </style>
-        </div>
-        <div>
-            <p class="parn">
-
-            </p>
-        </div>
-        <div class="imgs">
-            
-        </div>
+        <div> <h1 class="tiln1"> ${titulo2} </h1> </div>
+        <div> <p class="parn"> ${texto2} </p> </div>
+        <div style="background-image: url(${img2}); border-radius: 40px;"></div>
     </div>  
     <br>
     <div class="novidades-">
-        <div>
-            <h1 class="tiln2">
-                PBG mobile
-            </h1>
-            <style>
-                .tiln2 {
-                    color: white;
-                    font-family: Arial; 
-                }
-                .tiln2:hover {
-                    border-bottom: 2px solid white;
-                    width: 210px
-            }
-            </style>
-
-        </div>
-        <div>
-            <p class="parn">
-
-            </p>
-        </div>
-        <div class="imgs">
-            
-        </div>
+        <div> <h1 class="tiln2">${titulo3}</h1> </div>
+        <div> <p class="parn"> ${texto3}</p> </div>
+        <div style="background-image: url(${img3}); border-radius: 40px;"></div>
     </div> 
     <br> 
     <div class="novidades-">
-        <div>
-            <h1 class="tiln3">
-                Fortinit
-            </h1>
-            <style>
-                .tiln3 {
-                    color: white;
-                    font-family: Arial; 
-                }
-                .tiln3:hover {
-                    border-bottom: 2px solid white;
-                    width: 150px
-            }
-            </style>
-        </div>
-        <div>
-            <p class="parn">
-
-            </p>
-        </div>
-        <div class="imgs">
-            
-        </div>
+        <div> <h1 class="tiln3"> ${titulo4} </h1> </div>
+        <div> <p class="parn"> ${texto4} </p> </div>
+        <div style="background-image: url(${img4}); border-radius: 40px;"> </div>
     </div> 
     <br> 
     <div class="novidades-">
-        <div>
-            <h1 class="tiln4">
-                Legue of Legends
-            </h1>
-            <style>
-                .tiln4 {
-                    color: white;
-                    font-family: Arial; 
-                }
-                .tiln4:hover {
-                    border-bottom: 2px solid white;
-                    width: 330px
-            }
-            </style>
-        </div>
-        <div>
-            <p class="parn">
-
-            </p>
-        </div>
-        <div class="imgs">
-            
-        </div>
+        <div> <h1 class="tiln4"> ${titulo5}</h1> </div>
+        <div> <p class="parn"> ${texto5}</p> </div>
+        <div style="background-image: url(${img5}); border-radius: 40px;"></div>
     </div>  
     <br>
     <div class="novidades-">
-        <div>
-            <h1 class="tiln5">
-                Hill Climb
-            </h1>
-            <style>
-                .tiln5 {
-                    color: white;
-                    font-family: Arial; 
-                }
-                .tiln5:hover {
-                    border-bottom: 2px solid white;
-                    width: 190px
-            }
-            </style>
-        </div>
-        <div>
-            <p class="parn">
-
-            </p>
-        </div>
-        <div class="imgs">
-            
-        </div>
+        <div> <h1 class="tiln5"> ${titulo6} </h1> </div>
+        <div> <p class="parn"> ${texto6}</p> </div>
+        <div style="background-image: url(${img6}); border-radius: 40px;"></div>
     </div>  
     `;
 
