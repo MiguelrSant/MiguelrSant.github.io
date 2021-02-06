624fe8ab34187d25fca1349e7107006f23946faf
MiguelrSant <75269821+MiguelrSant@users.noreply.github.com>
2021-02-06 15:26:41 -0300

    New WebWord

--- script/ClickMobile.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $(".hamburguer").click(function () {
-      $(this).toggleClass("hamburguer-active");
-      $(".nav-list-mobile").toggleClass("active");
-    });
-  });
